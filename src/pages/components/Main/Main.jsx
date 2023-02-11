2576801f7686c840fc3a99dd8d54a130321f6bf6
Marko Malec <CroPsychooo@yahoo.com>
2023-02-11 02:34:17 +0100

    main component

---
+++ src/pages/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Sidebar from '../elements/Sidebar/Sidebar.jsx';
+
+const Main = () => {
+  return (
+      <Sidebar />
+  );
+};
+
+export default Main;
--- src/pages/components/elements/Sidebar/Sidebar.jsx
+++ src/pages/components/elements/Sidebar/Sidebar.jsx
@@ -1,35 +1,34 @@
-import React from 'react';
+import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
-
 const AuthShowcase = () => {
-    const { data: sessionData } = useSession();
-  
-    return (
-      <div className="flex flex-row items-center justify-center gap-4 ">
-        <p className="text-center text-2xl text-white">
-          {sessionData && <span>{sessionData.user?.name}</span>}
-        </p>
-        <button
-          className="text-white no-underline transition hover:bg-white/20"
-          onClick={sessionData ? () => void signOut() : () => void signIn()}
-        >
-          {sessionData ? "(Sign out)" : "Sign in"}
-        </button>
-      </div>
-    );
-  };
-  
+  const { data: sessionData } = useSession();
 
-export const Sidebar = ({game}) => {
-    return (
-        <aside className="w-64 h-screen border-r-2 border-slate-800">
-          <h1 className="text-2xl text-white font-bold text-center p-3 border-slate-800 border-b-2">{game.name}</h1>
-          {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
-          <AuthShowcase />
+  return (
+    <div className="flex flex-row items-center justify-center gap-4 ">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>{sessionData.user?.name}</span>}
+      </p>
+      <button
+        className="text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "(Sign out)" : "Sign in"}
+      </button>
+    </div>
+  );
+};
 
-        </aside>
-    )
-}
+const Sidebar = () => {
+  return (
+    <aside className="h-screen w-64 border-r-2 border-slate-800">
+      <h1 className="border-b-2 border-slate-800 p-3 text-center text-2xl font-bold text-white">
+        Test
+      </h1>
+      {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
+      <AuthShowcase />
+    </aside>
+  );
+};
 
 export default Sidebar;
