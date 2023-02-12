e5b5af1627d448f28d6fead625ae8dacb5247309
Marko Malec <CroPsychooo@yahoo.com>
2023-02-12 21:04:44 +0100

    queryClientProvider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Main from '../pages/components/Main/Main.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 
 const Home: NextPage = () => {
   return (
-    <>
+
+    <QueryClientProvider client={queryClient}>
      <Head>
         <meta name="description" content="The game" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main />
-    </>
+    </QueryClientProvider>
   );
 };
 
