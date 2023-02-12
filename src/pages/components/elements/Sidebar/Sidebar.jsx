cc4e004e70a841dec4720c007a68d07d226d8877
Marko Malec <CroPsychooo@yahoo.com>
2023-02-12 22:40:44 +0100

    changed styling

--- src/pages/components/elements/Sidebar/Sidebar.jsx
+++ src/pages/components/elements/Sidebar/Sidebar.jsx
@@ -8,27 +8,27 @@ const AuthShowcase = () => {
 
   return (
     <>
-    <div className="flex p-2 flex-row items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>{sessionData.user?.name}</span>}
-      </p>
-      <button
-        className="text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "(Sign out)" : "Sign in"}
-      </button>
-    </div>
-    <Menu />
+      <div className="flex flex-row items-center justify-center gap-4 p-2">
+        <p className="text-center text-2xl text-white">
+          {sessionData && <span>{sessionData.user?.name}</span>}
+        </p>
+        <button
+          className="text-white no-underline transition hover:bg-white/20"
+          onClick={sessionData ? () => void signOut() : () => void signIn()}
+        >
+          {sessionData ? "(Sign out)" : "Sign in"}
+        </button>
+      </div>
+      <Menu />
     </>
   );
 };
 
 const Sidebar = () => {
   return (
-    <aside className="h-screen w-64 border-r-2 border-slate-700 bg-slate-900">
-      <h1 className="border-b-2 border-slate-700 p-3 text-center text-2xl font-bold text-white">
-        { gameName }
+    <aside className="fixed mx-3 h-screen w-64 p-4 rounded-xl border-slate-700 bg-gray-50 bg-slate-900">
+      <h1 className="border-b border-slate-700 p-3 text-center text-2xl font-bold text-black">
+        {gameName}
       </h1>
       {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
       <AuthShowcase />
