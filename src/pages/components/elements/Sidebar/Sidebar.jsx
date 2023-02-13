f40dd5398876cc53ae2570feac6b04ca42901b3f
Marko Malec <CroPsychooo@yahoo.com>
2023-02-14 00:13:05 +0100

    Added next.js routes and Layout component to reuse componenets like sidebar

--- src/pages/components/elements/Sidebar/Sidebar.jsx
+++ src/pages/components/elements/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import Link from "next/link.js";
 import Menu from "./Nav/MainNav.jsx";
 const gameName = "The Game";
 
@@ -27,9 +28,11 @@ const AuthShowcase = () => {
 const Sidebar = () => {
   return (
     <aside className="fixed mx-3 h-screen w-64 p-4 rounded-xl border-slate-700 bg-slate-800">
+      <Link href="/">
       <h1 className="border-b border-slate-700 p-3 text-center text-2xl font-bold text-black">
         {gameName}
       </h1>
+      </Link>
       {/* {hello.data ? hello.data.greeting : "Loading tRPC query..."} */}
       <AuthShowcase />
     </aside>
