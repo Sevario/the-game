960409b6290be87330a91e2d6d1855ca586d2b6b
Marko Malec <CroPsychooo@yahoo.com>
2023-05-28 20:58:02 +0200

    Inventory

--- src/pages/components/elements/Sidebar/Nav/MainNav.tsx
+++ src/pages/components/elements/Sidebar/Nav/MainNav.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavSkill from "@sidebar/Nav/NavSkill";
+import NavCharacter from "./NavCharacter";
 
 const MainNav = () => {
-    return (
-        <div className="flex flex-col gap-4">
-                <NavSkill />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-4">
+      <NavSkill />
+      <NavCharacter />
+    </div>
+  );
 };
 
 export default MainNav;
---
+++ src/pages/components/elements/Sidebar/Nav/NavCharacter.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+import Inventory from "@components/Inventory/Inventory";
+
+const NavCharacter = () => {
+  return (
+    <>
+      <Link
+        href={`/inventory/Inventory`}
+        className="text-white no-underline transition hover:bg-white/20"
+      >
+        Inventory
+      </Link>
+    </>
+  );
+};
+
+export default NavCharacter;
